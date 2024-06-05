27fe231873cb82a45a9603880b1877ef92582cae
João pedro Lima <joaopedrofontinelelima99@gmail.com>
2024-06-04 21:41:08 -0300

    Feat:added responsive

--- menu.js
+++ menu.js
@@ -1,15 +1,19 @@
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+let btnFechar = document.querySelector('.btn-fechar');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+    overlay.classList.add('active');
+});
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+btnFechar.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    overlay.classList.remove('active');
+});
 
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+    overlay.classList.remove('active');
+});
