5b27b2458c1315ccbb9e2c3891f2a14affdf455f
João pedro Lima <joaopedrofontinelelima99@gmail.com>
2024-06-04 22:09:13 -0300

    Feat:close menu automatically

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ let btnMenu = document.getElementById('btn-menu');
 let menu = document.getElementById('menu-mobile');
 let overlay = document.getElementById('overlay-menu');
 let btnFechar = document.querySelector('.btn-fechar');
+let menuLinks = document.querySelectorAll('#menu-mobile nav ul li a');
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
@@ -16,4 +17,12 @@ btnFechar.addEventListener('click', () => {
 overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
     overlay.classList.remove('active');
+});
+
+// Fechar menu ao clicar em um link
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('abrir-menu');
+        overlay.classList.remove('active');
+    });
 });
